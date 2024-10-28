6095b896ad820e9be16a50a63945dd23fa5e457b
kmio <2751347576@qq.com>
2024-10-28 16:03:46 +0800

    done

--- part1/1.1/src/App.jsx
+++ part1/1.1/src/App.jsx
@@ -8,41 +8,55 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  const parts = [
+    { name: "Fundamentals of React", count: "10" },
+    { name: "Using props to pass data", count: "7" },
+    { name: "State of a component", count: "14" }
+  ]
 
   return (
     <div>
-      <Head course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Head course={course} />
+      <Content parts={parts} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
-const Head=(props)=>{
-  return(<Fragment>
+const Part = ({ name, count }) => {
+  return (
+    <>
+      <p>{name} {count}</p>
+    </>
+  )
+
+}
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      <Part name={parts[0].name} count={parts[0].count} />
+
+      <Part name={parts[1].name} count={parts[1].count} />
+
+      <Part name={parts[2].name} count={parts[2].count} />
+
+    </>
+  )
+
+}
+const Head = (props) => {
+  return (<Fragment>
     <h1>{props.course}</h1>
   </Fragment>)
-  
+
 }
-const Content=(props)=>{
-  return( <>
-    <p>
-       {props.part1} {props.exercises1}
-     </p>
-     <p>
-       {props.part2} {props.exercises2}
-     </p>
-     <p>
-       {props.part3} {props.exercises3}
-     </p>
- </>)
- 
-}
+
 const Total = (props) => {
-return(  <>
-  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    </>)
+  return (<>
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  </>)
 }
-  
+
 
 
 export default App
